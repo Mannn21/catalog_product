b7731e57a83c1d8901de9502485cbe9b07759300
Mannn21 <aimannurrofi21@gmail.com>
2023-07-20 21:33:26 +0700

    update search feature

--- src/components/CardList/index.jsx
+++ src/components/CardList/index.jsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect, Suspense, useRef } from "react";
+import React, { useState, useEffect, Suspense } from "react";
 import { getData } from "../../utils/getDatas.js";
 import {
-	ContentContainer,
-	ContentWrapper,
-	Content,
-	CardWrapper,
+  ContentContainer,
+  ContentWrapper,
+  Content,
+  CardWrapper,
 } from "./styled.js";
 import Skeleton from "../Skeleton";
 import PropTypes from "prop-types";
@@ -12,61 +12,52 @@ import PropTypes from "prop-types";
 const Card = React.lazy(() => import("../Card"));
 
 const CardList = ({ query }) => {
-	const [products, setProducts] = useState([]);
-	const prevProductsRef = useRef([]);
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
 
-	useEffect(() => {
-		const fetchApi = async () => {
-			const response = await getData();
-			setProducts(response.data);
-			prevProductsRef.current = response.data;
-		};
+  useEffect(() => {
+    const fetchApi = async () => {
+      const response = await getData();
+      setProducts(response.data);
+    };
 
-		fetchApi();
-	}, []);
+    fetchApi();
+  }, []);
 
-	useEffect(() => {
-		const fetchData = () => {
-			const search = prevProductsRef.current.filter(item =>
-				item.title.toLowerCase().includes(query.toLowerCase())
-			);
-			setProducts(search);
-		};
+  useEffect(() => {
+    if (query.length > 2) {
+      const search = products.filter((item) =>
+        item.title.toLowerCase().includes(query.toLowerCase())
+      );
+      setFilteredProducts(search);
+    } else {
+      setFilteredProducts(products);
+    }
+  }, [query, products]);
 
-		if (query.length > 2) {
-			fetchData();
-		} else if (query.length === 0) {
-			setProducts(prevProductsRef.current);
-		}
-	}, [query]);
-
-	return (
-		<ContentContainer>
-			{products.length === 0 ? (
-				<>
-					<h1>No Data Result...</h1>
-				</>
-			) : (
-				<ContentWrapper>
-					<Content style={{ display: "flex", gap: "50px", flexWrap: "wrap" }}>
-						{products?.map((product, index) => {
-							return (
-								<CardWrapper key={index}>
-									<Suspense fallback={<Skeleton />}>
-										<Card data={product} />
-									</Suspense>
-								</CardWrapper>
-							);
-						})}
-					</Content>
-				</ContentWrapper>
-			)}
-		</ContentContainer>
-	);
+  return (
+    <ContentContainer>
+      <ContentWrapper>
+        <Content style={{ display: "flex", gap: "50px", flexWrap: "wrap" }}>
+          {filteredProducts.length > 0 ? (
+            filteredProducts.map((product, index) => (
+              <CardWrapper key={index}>
+                <Suspense fallback={<Skeleton />}>
+                  <Card data={product} />
+                </Suspense>
+              </CardWrapper>
+            ))
+          ) : (
+            <div>No results found....</div>
+          )}
+        </Content>
+      </ContentWrapper>
+    </ContentContainer>
+  );
 };
 
 CardList.propTypes = {
-	query: PropTypes.string.isRequired,
+  query: PropTypes.string.isRequired,
 };
 
 export default CardList;
